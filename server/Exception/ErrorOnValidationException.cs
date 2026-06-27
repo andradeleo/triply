4eb38f0733b491925b6cfbf34f40e0ba661d16c9
Leonardo <contato.leoo0@gmail.com>
2026-06-27 13:08:59 -0300

    feat: add global exception handling with custom exceptions

---
+++ server/Exception/ErrorOnValidationException.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace Exception
+{
+    public class ErrorOnValidationException(List<string> errorMessages) : ExceptionBase(String.Empty)
+    {
+        private readonly List<string> _errors = errorMessages;
+
+        public override int StatusCode => (int)HttpStatusCode.BadRequest;
+
+        public override List<string> GetErrors()
+        {
+            return _errors;
+        }
+    }
+}
---
+++ server/Exception/ExceptionBase.cs
@@ -0,0 +1,9 @@
+﻿namespace Exception
+{
+    public abstract class ExceptionBase(string message) : SystemException(message)
+    {
+        public abstract int StatusCode { get; }
+
+        public abstract List<string> GetErrors();
+    }
+}
