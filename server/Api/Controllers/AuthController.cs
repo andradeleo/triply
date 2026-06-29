54f882f6761544dfe35973ffb7051c24c636831e
Leonardo <contato.leoo0@gmail.com>
2026-06-28 22:28:46 -0300

    refactor: register use cases via DI instead of manual instantiation

--- server/Api/Controllers/AuthController.cs
+++ server/Api/Controllers/AuthController.cs
@@ -1,19 +1,16 @@
 ﻿using Application.UseCases.Authentication;
 using Communication.Request;
-using Domain.Security;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class AuthController(IAccessTokenGenerator tokenService) : ControllerBase
+    public class AuthController : ControllerBase
     {
         [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody] RequestLogin request)
+        public async Task<IActionResult> Login([FromServices] ILoginUseCase useCase, [FromBody] RequestLogin request)
         {
-            var useCase = new LoginUseCase(tokenService);
-
             var response = await useCase.Execute(request);
 
             return Ok(response);
