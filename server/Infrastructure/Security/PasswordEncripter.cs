9083d5b09175c56756087ea10f6d862f2b9cc3cf
Leonardo <contato.leoo0@gmail.com>
2026-06-28 22:28:56 -0300

    feat: hash password with BCrypt on login

---
+++ server/Infrastructure/Security/PasswordEncripter.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Security;
+using BC = BCrypt.Net.BCrypt;
+
+namespace Infrastructure.Security
+{
+    public class PasswordEncripter : IPasswordEncripter
+    {
+        public string Encrypt(string password)
+        {
+            string passwordHash = BC.HashPassword(password);
+
+            return passwordHash;
+        }
+
+        public bool Verify(string password, string passwordHash)
+        {
+            return BC.Verify(password, passwordHash);
+        }
+    }
+}
